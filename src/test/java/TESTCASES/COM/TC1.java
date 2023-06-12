8739433249ba5bc280bff5e8e0b313317368f46f
HP <HP@DESKTOP-4LD2RS8>
2023-06-12 14:39:00 +0530

    this is my first commit.....

---
+++ src/test/java/TESTCASES/COM/TC1.java
@@ -0,0 +1,14 @@
+package TESTCASES.COM;
+
+import org.testng.Reporter;
+
+
+public class TC1 {
+	
+	public void m1()
+	{
+		Reporter.log("TC1 Passed",true);
+	}
+	
+
+}
---
+++ src/test/java/TESTCASES/COM/TC2.java
@@ -0,0 +1,12 @@
+package TESTCASES.COM;
+
+import org.testng.Reporter;
+
+public class TC2 {
+	
+	public void m2()
+	{
+		Reporter.log("TC2 passed",true);
+	}
+
+}
